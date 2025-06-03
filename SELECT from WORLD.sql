fcface3a182caf7cbfc168a6e44cbb6fd52b1422
Kamila de Souza Vieira <41303586+kamilavieira@users.noreply.github.com>
2025-06-03 16:20:51 -0300

    Update SELECT from WORLD.sql
    A primeira parte (questões de 1 a 8) foi feita na aula ao vivo e a segunda parte (questões de 9 a 13) foi feita depois da aula. Perdão pela bagunça professor, mas não sabia se era pra criar um novo arquivo com o mesmo nome com essa atividade completa ou se era pra continuar no mesmo arquivo. De toda forma, eu separei em duas partes.

--- SELECT from WORLD.sql
+++ SELECT from WORLD.sql
@@ -24,3 +24,34 @@ WHERE area >= 3000000 OR population >= 250000000
 --8 questao
 SELECT name, population, area FROM world
 WHERE area >= 3000000 XOR population >= 250000000
+
+
+
+--RESTANTE DA ATIVIDADE FEITO APÓS A AULA AO VIVO DE INTRODUÇÃO AO SQL
+--NOTA P/ MIM: o AS no SQL serve pra nomear uma variável temporária que armazena um valor, mas não muda nada no banco de dados original.
+--9 questao
+SELECT name, 
+  ROUND(population / 1000000, 2) AS population_millions,
+  ROUND(gdp / 1000000000, 2) AS gdp_billions
+FROM world
+WHERE continent = 'South America';
+--10 questao
+SELECT name, ROUND(gdp / population, -3) AS per_capita FROM world
+WHERE gdp >= 1000000000000 
+--11 questao
+SELECT name, capital
+  FROM world
+ WHERE LENGTH(name) = LENGTH(capital)
+--12 questao
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital, 1) AND name != capital
+--13 questao
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+  AND name LIKE '%e%'
+  AND name LIKE '%i%'
+  AND name LIKE '%o%'
+  AND name LIKE '%u%'
+  AND name NOT LIKE '% %';
